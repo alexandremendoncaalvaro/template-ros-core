cef5b8572d4892f70408c8ffbabc640098fe3eda
Alexandre <alexandre.alvaro@hotmail.com>
2020-01-29 14:39:47 +0300

    jyfhgfmn

--- packages/circle_drive/scripts/velocity_pub.py
+++ packages/circle_drive/scripts/velocity_pub.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
-import time
+
 import rospy
 from duckietown_msgs.msg import Twist2DStamped
+import time
 
 rospy.init_node('modified_velocity')
 
@@ -13,14 +14,12 @@ time.sleep(1)
 
 msg=Twist2DStamped()
 
-msg.v=1
-
 while not rospy.is_shutdown():
     msg.v = 3.0
     msg.omega = 0.0
     pub.publish(msg)
-    rate.sleep()
+    time.sleep()
     msg.v = 0.0
     msg.omega = 0.0
     pub.publish(msg)
-    rate.sleep()
+    time.sleep()
