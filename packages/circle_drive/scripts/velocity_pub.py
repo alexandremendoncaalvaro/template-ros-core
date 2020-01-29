96dd8fa21f17dd2f4ac7e7061757bb0b6be54f40
Alexandre <alexandre.alvaro@hotmail.com>
2020-01-29 12:07:49 +0300

    Test

---
+++ packages/circle_drive/scripts/velocity_pub.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+import time
+import rospy
+from duckitown_msg.msg import Twist2DStamped
+
+rospy.init_node('modified_velocity')
+
+pub=rospy.Publisher('/duckpi4/kinematics_node/velocity',
+                        Twist2DStamped,
+                        queue_size=10)
+
+time.sleep(1)
+
+msg=Twist2DStamped()
+
+msg.v=1
+
+pub.publish(msg)
