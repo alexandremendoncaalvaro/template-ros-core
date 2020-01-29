88aa9aa4c4f610c06b4902dfd43fe038ce414d12
Alexandre <alexandre.alvaro@hotmail.com>
2020-01-29 12:50:43 +0300

    asdfasdf

--- packages/circle_drive/scripts/velocity_pub.py
+++ packages/circle_drive/scripts/velocity_pub.py
@@ -5,7 +5,7 @@ from duckitown_msg.msg import Twist2DStamped
 
 rospy.init_node('modified_velocity')
 
-pub=rospy.Publisher('/duckpi4/kinematics_node/velocity',
+pub=rospy.Publisher('/modified_velocity/velocity',
                         Twist2DStamped,
                         queue_size=10)
 
@@ -16,5 +16,11 @@ msg=Twist2DStamped()
 msg.v=1
 
 while not rospy.is_shutdown():
-	pub.publish(msg)
-	time.sleep(1)
+    msg.v = 3.0
+    msg.omega = 0.0
+    pub.publish(msg)
+    rate.sleep()
+    msg.v = 0.0
+    msg.omega = 0.0
+    pub.publish(msg)
+    rate.sleep()
