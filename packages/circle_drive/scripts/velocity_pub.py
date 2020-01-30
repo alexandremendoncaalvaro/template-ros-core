9c7f17f85bc39507dbaf930a43fabf90a334c944
Alexandre <alexandre.alvaro@hotmail.com>
2020-01-30 11:33:07 +0300

    fgxnsdbv,n.f;mçvsl/dx

--- packages/circle_drive/scripts/velocity_pub.py
+++ packages/circle_drive/scripts/velocity_pub.py
@@ -6,7 +6,7 @@ import time
 
 rospy.init_node('modified_velocity')
 
-pub=rospy.Publisher('/duckpi4/joy_mapper_node/car_cmd',
+pub=rospy.Publisher('/duckpi4/lane_controller_node/car_cmd',
                         Twist2DStamped,
                         queue_size=10)
 
@@ -15,7 +15,7 @@ time.sleep(1)
 msg=Twist2DStamped()
 
 while not rospy.is_shutdown():
-    msg.v = 3.0
+    msg.v = 0.2
     msg.omega = 0.0
     pub.publish(msg)
     time.sleep(1)
